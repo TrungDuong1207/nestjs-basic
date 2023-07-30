9eddad6bf2254d34d09a01d40865101f71eb6a9d
Trung Duong <trungdt.cmaxx@gmail.com>
2023-07-30 16:13:01 +0700

    cai dat config bien env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://trung:Trung142696!@cluster0.z5ztbaa.mongodb.net/test')],
+  imports: [
+    // MongooseModule.forRoot('mongodb+srv://trung:Trung142696!@cluster0.z5ztbaa.mongodb.net/test'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
