070ef758950290e3c4a61f82409cc4e939ad71c1
Trung Duong <trungdt.cmaxx@gmail.com>
2023-08-12 20:49:58 +0700

    message decorator

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { TransformInterceptor } from './core/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -14,12 +15,14 @@ async function bootstrap() {
 
   const reflector = app.get(Reflector);
   app.useGlobalGuards(new JwtAuthGuard(reflector));
+  app.useGlobalInterceptors(new TransformInterceptor(reflector));
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
 
   app.useGlobalPipes(new ValidationPipe());
+  
   //config cors
   app.enableCors(
     {
