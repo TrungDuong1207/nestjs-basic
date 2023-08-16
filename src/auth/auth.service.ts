d3e9a480a4706c5e55e909e6f92a65bfd0b1f147
Trung Duong <trungdt.cmaxx@gmail.com>
2023-08-16 20:48:53 +0700

    tao refresh token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,12 +6,15 @@ import { RegisterUserDto } from 'src/users/dto/create-user.dto';
 import { User } from 'src/users/schemas/user.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { ConfigService } from '@nestjs/config';
+import ms from 'ms';
 
 @Injectable()
 export class AuthService {
     constructor(
         private usersService: UsersService,
         private jwtService: JwtService,
+        private configService: ConfigService,
         @InjectModel(User.name) private userModel: Model<User>
     ) { }
 
@@ -36,18 +39,22 @@ export class AuthService {
             email,
             role
         };
+        const refresh_token = this.createRefreshToken({name: "trung"})
         return {
             access_token: this.jwtService.sign(payload),
-            _id,
-            name,
-            email,
-            role
+            refresh_token,
+            user: {
+                _id,
+                name,
+                email,
+                role
+            }
         };
     }
 
     async register(registerUserDto: RegisterUserDto) {
-        const isExist = this.userModel.findOne({email: registerUserDto.email});
-        if(isExist) {
+        const isExist = this.userModel.findOne({ email: registerUserDto.email });
+        if (isExist) {
             throw new BadRequestException(`the email ${registerUserDto.email} da ton tai tren he thong`);
         }
         const hashPassword = this.usersService.getHashPassword(registerUserDto.password)
@@ -57,5 +64,13 @@ export class AuthService {
             role: "USER"
         })
         return user;
+    }
+
+    createRefreshToken = (payload) => {
+       const refresh_token = this.jwtService.sign(payload, {
+        secret: this.configService.get<string>("JWT_REFRESH_TOKEN_SECRET"),
+        expiresIn: ms(this.configService.get<string>("JWT_REFRESH_EXPIRE"))/1000 //vì expireIn tinh theo giây mà thằng ms nó tính theo miligiay
+       });
+    return refresh_token
     }
 }
